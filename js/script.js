71ac74c6cae0b72918b738277ab3ba579dc64e41
Duma Oleg <dumik.alive@gmail.com>
2018-03-10 18:14:55 +0200

    added close button munu and page of catalog

--- js/script.js
+++ js/script.js
@@ -7,4 +7,39 @@ $(document).ready(function(){
      $('body,html').animate({scrollTop: top}, 2000);
  });
 });
+;
+
+
+
+
+var menuOpen = function(openMenu, openClose) {
+	var openMenu = document.getElementById('open-menu');
+	var closeMenu =  document.getElementById('close-menu');
+		
+		document.body.style.overflow = "hidden";
+
+		openMenu.style.display = "none";
+		closeMenu.style.display = "block";
+
+
+		return  closeMenu;
+};
+
+var menuClose = function() {
+	var openMenu = document.getElementById('open-menu');
+	var closeMenu =  document.getElementById('close-menu');
+		
+		document.body.style.overflow = "auto";
+
+		openMenu.style.display = "block";
+		closeMenu.style.display = "none";
+
+
+		return openMenu;
+};
+
+var menuCloseBody = function() {
+
+}
+
 
